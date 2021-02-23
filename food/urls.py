17282644fc2d04f1ef6793d738fc17fbbc35ae4e
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-23 10:18:25 +0430

    links for description created

--- food/urls.py
+++ food/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('<int:item_id>/', views.detail, name="detail"),
 ]
--- food/views.py
+++ food/views.py
@@ -7,11 +7,27 @@ from .models import item
 
 
 
+# def index(request):
+#     item_lst = item.objects.all()
+#     template = loader.get_template('./food/index.html')
+#     context = {
+#         'item_lst': item_lst,
+#     }
+#     return HttpResponse(template.render(context, request))
+
 def index(request):
     item_lst = item.objects.all()
-    template = loader.get_template('./food/index.html')
+    # template = loader.get_template('./food/index.html')
     context = {
         'item_lst': item_lst,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, './food/index.html', context)
+
+def detail(request, item_id):
+    item_ = item.objects.get(pk=item_id)
+    context = {
+        'item': item_
+    }
+    return render(request, 'food/index.html', context)
+    
 
