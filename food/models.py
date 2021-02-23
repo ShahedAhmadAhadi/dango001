45887a38a8687c43779e415bee7b7fe74aa70179
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-23 08:50:25 +0430

    first models created

--- food/models.py
+++ food/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class item(models.Model):
+    item_name = models.TextField(max_length=200)
+    item_desc = models.TextField(max_length=200)
+    item_price = models.IntegerField()
+
