caf44650110753c6bb9eb10a336bf5a7bafdb01c
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-23 09:06:21 +0430

    template: data rendered from database

--- food/views.py
+++ food/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import item
 
 # Create your views here.
 
+
+
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    item_lst = item.objects.all()
+    template = loader.get_template('./food/index.html')
+    context = {
+        'item_lst': item_lst,
+    }
+    return HttpResponse(template.render(context, request))
+
