65dfa24130c6dfaa4e5cf51e1729d9021c0075a8
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-27 17:22:05 +0430

    update functionality added

--- food/views.py
+++ food/views.py
@@ -39,5 +39,16 @@ def create_item(request):
         return redirect('food:index')
 
     return render(request, 'food/item-form.html', {'form':form})
+
+def update_item(request, id):
+    item = Item.objects.get(id = id)
+    form = Item_form(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request, 'food/item-form.html', {'form':form, 'item':item})
+
     
 
