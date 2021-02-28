3bbf9c0ce62332017c2c77113c651943460f71af
Shahed Ahmad <shaheda.a16@gmail.com>
2021-02-28 08:14:25 +0430

    added email field to users

--- users/views.py
+++ users/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import Register
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = Register(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, you acount is created')
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = Register()
     return render(request, 'users/register.html', {'form': form})
 
 
